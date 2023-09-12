a00a86d8fe3b050d93072626101da0d712a5123b
Taha Iqbal <tahaiqbal001@gmail.com>
2023-09-12 12:52:17 +0500

    Generated HTML using JS

---
+++ Scripts/amazon.js
@@ -0,0 +1,48 @@
+let productsHTML = '';
+
+products.forEach((product) => {
+  productsHTML += `
+    <div class="product-container">
+      <div class="product-image-container">
+        <img class="product-image" src="${product.image}">
+      </div>
+
+      <div class="product-name limit-text-to-2-lines">
+        ${product.name}
+      </div>
+
+      <div class="product-rating-container">
+        <img class="product-rating-stars" src="images/ratings/rating-${product.rating.stars * 10}.png">
+        <div class="product-rating-count link-primary">
+          ${product.rating.count}
+        </div>
+      </div>
+
+      <div class="product-price">
+        $${(product.priceCents / 100).toFixed(2)}
+      </div>
+
+      <div class="product-quantity-container">
+        <select>
+          <option selected value="1">1</option>
+          <option value="2">2</option>
+          <!-- Add more options as needed -->
+        </select>
+      </div>
+
+      <div class="product-spacer"></div>
+
+      <div class="added-to-cart">
+        <img src="images/icons/checkmark.png">
+        Added
+      </div>
+
+      <button class="add-to-cart-button button-primary">
+        Add to Cart
+      </button>
+    </div>
+  `;
+});
+
+// Append the generated HTML content to the '.js-products-containers' element(s)
+document.querySelector('.js-products-containers').innerHTML = productsHTML;
